e1c9ba158f8dd9e004733a3d3ce057261441ca21
NEERAJ TADHIYAL <tadhiyalne@gmail.com>
2022-04-21 23:04:14 +0530

    A small Quality of Life Improvement XD

--- AA Replica/Assets/Scripts/GameManager.cs
+++ AA Replica/Assets/Scripts/GameManager.cs
@@ -22,6 +22,10 @@ public class GameManager : MonoBehaviour
   {
     Win();
   }
+    if(Input.GetKeyDown(KeyCode.Escape))
+    {
+      SceneManager.LoadScene(0);
+    }
   }
   public void End()
   {
